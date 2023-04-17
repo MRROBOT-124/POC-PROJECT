21fcf98536e1a4281a0eadf8c1a06b1ad5048f45
MRROBOT-124 <saiamith376@gmail.com>
2023-04-17 16:39:04 +0530

    ADDED TEST HELLO WORLD CONTROLLER

--- src/main/java/com/realtime/project/controller/ApplicationController.java
+++ src/main/java/com/realtime/project/controller/ApplicationController.java
@@ -7,11 +7,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controller Class used for
+ * providing rest end points
+ */
 @RestController
 @CrossOrigin(value = "*")
 @RequestMapping(value = "/v1")
 public class ApplicationController {
 
+    /**
+     * Simple API that returns
+     * Hello World! response
+     * @return HELLO WORLD!
+     */
     @GetMapping(value = "/hello")
     public ResponseEntity<String> helloWorld() {
         return new ResponseEntity<>("Hello World!", HttpStatus.OK);
